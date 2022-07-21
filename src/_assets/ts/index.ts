28d5a6de8243097ed73217850fee4279a33584cf
Pandu Supriyono <mail@supriyono.nl>
2022-07-21 15:02:33 +0200

    refactor: disable visitor count script (for now)

--- src/_assets/ts/index.ts
+++ src/_assets/ts/index.ts
@@ -2,7 +2,6 @@ import DarkModeToggle from './DarkModeToggle'
 import UtrechtTime from './UtrechtTime'
 import PortraitReveal from './PortraitReveal'
 import NowPlaying from './NowPlaying'
-import VisitorCount from './VisitorCount'
 import Switcher from './Switcher'
 
 export function init () {
@@ -15,10 +14,6 @@ export function init () {
     document.querySelector('[data-module="portrait-reveal"]')
   )
   new NowPlaying(document.querySelector('[data-module="now-playing"]'))
-  new VisitorCount(document.querySelector('[data-module="visitor-count"]'), {
-    namespace: process.env.VISITOR_COUNT_NAMESPACE,
-    key: process.env.VISITOR_COUNT_KEY
-  })
 
   Array.from(document.querySelectorAll('[data-switcher]')).forEach(
     (element) => new Switcher(element)
